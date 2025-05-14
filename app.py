c728fe69923b6b16bdbd0ec787a3b9949d459464
Prashanth <prashanth2001v@gmail.com>
2025-05-14 15:22:43 +0530

    Ensure app.py is in the correct location

--- app.py
+++ app.py
@@ -329,6 +329,7 @@ def logout():
 
 # Run the app
 if __name__ == '__main__':
-    port = int(os.environ['PORT'])
-    app.run(host="0.0.0.0", port=port)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
